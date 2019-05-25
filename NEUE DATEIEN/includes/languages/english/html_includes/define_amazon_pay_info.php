b15a3c36662889ab32d579ed1238b6e54570c32c
webchills <office@webchills.com>
2019-05-25 11:36:39 +0200

    2.2.3 Release

--- NEUE DATEIEN/includes/languages/english/html_includes/define_amazon_pay_info.php
+++ NEUE DATEIEN/includes/languages/english/html_includes/define_amazon_pay_info.php
@@ -7,6 +7,6 @@ Or the message "To continue shopping, your browser needs access to Amazon Pay" a
 At the top left of the address bar is a shield symbol:<br />
 <img src="images/firefox-amazon-2-en.jpg" alt="shield symbol" border="0" /><br /><br />
 Mouseover shows that Firefox has blocked elements.<br /><br />
-Click on the shield icon and then disable blocking for this site:<br />
-<img src="images/firefox-amazon-3-en.jpg" alt="disable blocking for this site" border="0" /><br /><br />
+Click on the shield icon and then Turn off Blocking for This Site:<br />
+<img src="images/firefox-amazon-3-en.jpg" alt="Turn off Blocking for This Site" border="0" /><br /><br />
 Then the Amazon window will be loaded immediately and you can continue the checkout.</p>
